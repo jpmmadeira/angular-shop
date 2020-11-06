9cb8215531f1010fc1469d298da10555a8a96abc
jpmendesmadeira <joaomadeira@outlook.pt>
2020-11-06 23:09:15 +0000

    Product update added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -34,6 +34,7 @@ import { MatSortModule } from "@angular/material/sort";
 
 import localePt from "@angular/common/locales/pt";
 import { registerLocaleData } from "@angular/common";
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 
 registerLocaleData(localePt);
 
@@ -49,6 +50,7 @@ registerLocaleData(localePt);
     RedDirective,
     ForDirective,
     ProductReadComponent,
+    ProductUpdateComponent,
   ],
   imports: [
     BrowserModule,
